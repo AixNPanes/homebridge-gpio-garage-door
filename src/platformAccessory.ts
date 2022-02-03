079780f9f8cc2a6d87a7aca482a4547b9ee12c54
AixNPanes <timdaley@earthling.net>
2022-02-03 11:16:42 -0500

    rename project

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,6 +1,6 @@
 import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge';
 
-import { ExampleHomebridgePlatform } from './platform';
+import { GPIOGarageDoorOpener } from './GPIOGarageDoorOpenere';
 
 /**
  * Platform Accessory
@@ -20,7 +20,7 @@ export class ExamplePlatformAccessory {
   };
 
   constructor(
-    private readonly platform: ExampleHomebridgePlatform,
+    private readonly platform: GPIOGarageDoorOpener,
     private readonly accessory: PlatformAccessory,
   ) {
 
