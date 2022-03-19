d4d2e40adec500b8245ba6dcc32fa37585970e3b
AixNPanes <timdaley@earthling.net>
2022-03-19 12:28:15 -0400

    fix plugin name and node version requirement

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,7 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'GPIOGarageDoorOpener';
+export const PLATFORM_NAME = 'GPIO Garage Door Opener';
 
 /**
  * This must match the name of your plugin as defined the package.json
